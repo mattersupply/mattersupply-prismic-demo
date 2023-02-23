1a654fc9dc8cdb798d240548874582043b2bf951
David G. Fonnegra <fonne@mattersupply.co>
2023-02-23 09:52:59 -0500

    Adding props with children

--- src/utils/rich-text.tsx
+++ src/utils/rich-text.tsx
@@ -2,10 +2,16 @@ import React from "react";
 import { Description, HeroTitle, Title } from "@/styled";
 
 export const richTextComponents = {
-  heading1: ({ children }) => <Title>{children}</Title>,
-  paragraph: ({ children }) => <Description>{children}</Description>,
+  heading1: ({ children }: React.PropsWithChildren) => (
+    <Title>{children}</Title>
+  ),
+  paragraph: ({ children }: React.PropsWithChildren) => (
+    <Description>{children}</Description>
+  ),
 };
 
 export const originalComponents = {
-  heading1: ({ children }) => <HeroTitle>{children}</HeroTitle>,
+  heading1: ({ children }: React.PropsWithChildren) => (
+    <HeroTitle>{children}</HeroTitle>
+  ),
 };
